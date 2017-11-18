f8c63cdfaaa3519841fed6ccaa2017ae2c190a2e
Maria Zorkaltseva <fear2029@gmail.com>
2017-11-19 04:20:27 +0700

    Initial Commit

---
+++ week1/week1 - Степень двойки.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 19 01:36:21 2017
+
+author: Maria Zorkaltseva
+"""
+
+# Формат ввода
+# Вводится целое неотрицательное число N. N≤100.
+# Формат вывода
+# Выведите 2ᴺ.
+
+N = int(input())
+print(2 ** N)
